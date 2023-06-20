e98124a6591d85e14b91c43e3422b1905c5e41c0
Ayden Diel <aydendiel@gmail.com>
2023-06-20 15:07:43 -0400

    started working on sexp->html macro

--- main.rkt
+++ main.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require (for-syntax racket/match))
 ;; GOAL: convert s-expression html to
 ;; calls to this function:
 ;   (create-element component #:props [props null] . children)
@@ -11,6 +12,29 @@
 ;         (<el header)
 ;         (<el gif-controller #:props ($/obj [src "./res/cat_dancing.webm"]))))
 
-; (define test-element
-;     (sexp->element `(div ([className "test"]))))
+; (define-syntax (our-if stx)
+;   (match (syntax->list stx)
+;    [(list name condition true-expr false-expr)
+;     (datum->syntax stx `(cond [(invert-boolean ,condition) (our-if (not ,condition) ,true-expr ,false-expr)]
+;                               [else ,false-expr]))]))
 
+; (define example-sexp '("p" "hello world"))
+
+; (displayln example-sexp)
+
+; (match example-sexp
+;   [(list tag-name child) (printf "~a ~a\n" tag-name child)]
+;   [_ (printf "pattern not matched\n")])
+
+(define-syntax (sexp->html stx)
+  (define expr (syntax->datum stx))
+  (displayln expr)
+  (define (match-expr expr)
+    (match expr
+      [(? string? str) (datum->syntax stx `(format "~a" ,str))]
+      [(? number? num) (datum->syntax stx `(format "~d" ,num))]
+      [(list _ (list (app symbol->string tag-name) (app match-expr child))) (datum->syntax stx `(format "<~a>~a</~a>" ,tag-name ,child ,tag-name))]))
+  (match-expr expr)
+  )
+
+(sexp->html (div (p "hello world")))
